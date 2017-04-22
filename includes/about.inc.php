516d648c4b53e56e0ef885651983c9c9a15dab91
Fluffy <gummifiedfluff@gmail.com>
2017-04-22 15:43:30 -0400

    Separate about page into .inc file

---
+++ includes/about.inc.php
@@ -0,0 +1,50 @@
+<div class="block">
+	<h3>Overview</h3>
+	<ul>
+		<li>Receive browser notifications when specified stalkees join or leave a Teeworlds server (both 0.5 and 0.6 versions).</li>
+	</ul>
+</div>
+<div class="block">
+	<h3>Settings</h3>
+	<ul>
+		<li>All settings are stored in the local browser storage. Thus, your settings are saved and loaded during each session, provided that you use the same browser.</li>
+		<div class="hiddenTextWrap item">
+			<h4 class="header">Detailed Information</h4>
+			<div class="hidden">
+				<ul class="settingsDetailedList">
+					<li>
+						<h3 class="hiddenHead1">Minimum threshold for loaded servers.</h3>
+						<p class="hiddenParagraph1">If the total number of teeworlds servers found is below the specified threshold, then the fetched data is ignored and a new round of server requests is sent. The lower the setting is, the more false alarms you will receive.</p>
+					</li>
+					<li>
+						<h3 class="hiddenHead1">Tolerance for failed servers</h3>
+						<p class="hiddenParagraph1">Similar to the "Minimum threshold for loaded servers" setting, this places a threshold on number of failed server requests. The higher the percentage is set, the more false alarms you receive.</p>
+					</li>
+					<li>
+						<h3 class="hiddenHead1">Load Defaults</h3>
+						<p class="hiddenParagraph1">Replace all settings with their defaults. The stalkee list is preserved.</p>
+					</li>
+					<li>
+						<h3 class="hiddenHead1">Clear All</h3>
+						<p class="hiddenParagraph1">Delete all local storage, wiping both settings and stalkees.</p>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</ul>
+</div>
+<div class="block">
+	<h3>Miscellaneous</h3>
+	<ul>
+		<li>If there's more than one instance of a stalkee online, only one of them is displayed.</li>
+		<li>You can't have stalkees with the same nickname.</li>
+	</ul>
+</div>
+<div class="block">
+	<h3>About</h3>
+	<ul>
+		<li>Fork this project on <a href="https://github.com/Piepow/TeeStalker">GitHub</a>.</li>
+		<li>Feel free to PM me on the <a href="https://forum.ddnet.tw/memberlist.php?mode=viewprofile&u=531">DDNet Forum</a> or on the <a href="https://www.teeworlds.com/forum/profile.php?id=126896">Teeworlds Forum</a>.</li>
+		<li>You can usually find me ingame on Teeworlds with the nickname 'Hey, fuck you!'</li>
+	</ul>
+</div>
